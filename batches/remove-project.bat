637727269440bd6f430f2137dc666732a5ae1c6d
agnoam <agnoam.aghai.ooo.123@gmail.com>
2021-04-21 12:32:45 +0300

    added volume test

--- batches/remove-project.bat
+++ batches/remove-project.bat
@@ -3,6 +3,8 @@ echo Removing all project's data
 kubectl delete deployment socket-handler redis kibana elasticsearch
 kubectl delete service socket-handler-service redis-service kibana-service elasticsearch-service
 @REM kubectl delete secret rabbitmq-secrets
+kubectl delete pvc elasticsearch-volume-claim
+kubectl delete pv elasticsearch-volume
 kubectl delete configmap kibana-configmap
 
 echo All data removed
